97325d272ed370dd87c70b6422b32e63ee035307
waywedesign <ajithdev.c@waywedesign.com>
2023-02-20 11:08:51 +0530

    Change Note:Form

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -49,7 +49,7 @@ const Hero = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 0 24 24"
                  fill="currentColor" className="w-8 h-8">
-                <path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clip-rule="evenodd" />
+                <path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
               </svg>
 
                 <p className='text-xs'>Add more</p>
