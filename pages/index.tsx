7577e7d991c2f4d13adb34ad4e5fcf2fdd649bd4
waywedesign <ajithdev.c@waywedesign.com>
2023-02-20 17:55:49 +0530

    development component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Hero from '@/components/Hero'
 import Form from '@/components/Form'
+import Development from '@/components/Development'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +19,7 @@ export default function Home() {
       <main>
         <Hero/>
         <Form/>
+        <Development/>
       
         
       </main>
