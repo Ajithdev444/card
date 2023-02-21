b711863007902e2f57038addfa3d9660f7c016b8
waywedesign <ajithdev.c@waywedesign.com>
2023-02-21 10:55:08 +0530

    content added

---
+++ pages/tools/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>Recently added</h1>
+                <p>The latest tools that help you and your team
+                    building software better,together.
+                </p>
+            </div>
+            <div>
+                <div>
+                    <h2>Instabug</h2>
+                    <p>The top apps in the world rely on instabug for bug
+                        reporting, crash reporting, and in-app user 
+                        feedback.
+                    </p>
+                </div>
+                <div>
+                    <h2>Octobox</h2>
+                    <p>Octobox helps you manage your notifications in 
+                        the same way Gmail helps you manage your 
+                        email.
+                    </p>
+                </div>
+                <div>
+                    <h2>Gammma Data Leak Prevention</h2>
+                    <p>69% of data branches start with negligence. one
+                        negligent employee could cost your company a 
+                        decade worth of reputataion.
+                    </p>
+                </div>
+                <div>
+                    <h2>DeepScan</h2>
+                    <p>DeepScan is an advanced static analysis tools
+                        engineered to support JavaScript, TypeSCRIPT,
+                        React, and Vue.js.
+                    </p>
+                </div>
+                <div>
+                    <h2>LeanBoard</h2>
+                    <p>Lean Board is a collaborative whiteboard with
+                         sticky notes which seamlessly connects with your
+                         GitHub issue.</p>
+                </div>
+                <div>
+                    <h2>ODIN</h2>
+                    <p>The top apps in the world relay on Instabug for bug
+                        reporting, crash reporting, and in-app user 
+                        feedback.
+                    </p>
+                </div>
+
+            </div>
+            <div>
+                <button>View all</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
