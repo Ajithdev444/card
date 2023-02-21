c51cc20314ca111c5b77af2dc730743d1b6e5393
waywedesign <ajithdev.c@waywedesign.com>
2023-02-21 16:28:14 +0530

    layout

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -1,61 +1,94 @@
 import React from 'react'
+import Image from 'next/image'
+import instabug from '../../public/instabug.png'
+import octobox from '../../public/octobox.png'
+import gamma from '../../public/gamma.jpg'
+import deepscan from '../../public/deepscan.png'
+import leanboard from '../../public/leanboard.jpg'
+import ocd from '../../public/ocd.jpg'
 
 const index = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <h1>Recently added</h1>
-                <p>The latest tools that help you and your team
+    <div className='bg-[#10182F] h-screen flex justify-center items-center'>
+        <div className='bg-white h-4/5 w-10/12 rounded-md'>
+            <div className='text-center'>
+                <h1 className='text-base font-bold' >Recently added</h1>
+                <p className='text-xs'>The latest tools that help you and your team
                     building software better,together.
                 </p>
             </div>
-            <div>
-                <div>
-                    <h2>Instabug</h2>
-                    <p>The top apps in the world rely on instabug for bug
+            <div className='grid grid-cols-2 p-16 gap-8'>
+                <div className='flex items-center lg:w-4/6 gap-4'>
+                    <Image src={instabug} alt='instabug' className='w-10 h-10 rounded-full'/>
+                    <div>     
+                    <h2 className='text-sm font-semibold'>Instabug</h2>
+                    <p className='text-xs'>The top apps in the world rely on instabug for bug
                         reporting, crash reporting, and in-app user 
                         feedback.
                     </p>
+                    </div>  
                 </div>
-                <div>
-                    <h2>Octobox</h2>
-                    <p>Octobox helps you manage your notifications in 
-                        the same way Gmail helps you manage your 
-                        email.
-                    </p>
-                </div>
-                <div>
-                    <h2>Gammma Data Leak Prevention</h2>
-                    <p>69% of data branches start with negligence. one
-                        negligent employee could cost your company a 
-                        decade worth of reputataion.
-                    </p>
-                </div>
-                <div>
-                    <h2>DeepScan</h2>
-                    <p>DeepScan is an advanced static analysis tools
+
+                <div className='flex items-center lg:w-4/6 gap-4'>
+                    <Image src={deepscan} alt='deepscan' className='w-10 h-10 rounded-full'/>
+                    <div>
+                    <h2 className='text-sm font-semibold'>DeepScan</h2>
+                    <p className='text-xs'>DeepScan is an advanced static analysis tools
                         engineered to support JavaScript, TypeSCRIPT,
                         React, and Vue.js.
                     </p>
+                    </div>
                 </div>
-                <div>
-                    <h2>LeanBoard</h2>
-                    <p>Lean Board is a collaborative whiteboard with
+
+                <div className='flex items-center lg:w-4/6 gap-4'>
+                    <Image src={octobox} alt='octobox' className='w-10 h-10 rounded-full'/>
+                    <div>
+                    <h2 className='text-sm font-semibold'>Octobox</h2>
+                    <p className='text-xs'>Octobox helps you manage your notifications in 
+                        the same way Gmail helps you manage your 
+                        email.
+                    </p>
+                    </div>
+                   
+                </div>
+
+                <div className='flex items-center lg:w-4/6 gap-4'>
+                    <Image src={leanboard} alt='leanboard' className='w-10 h-10 rounded-full'/>
+                    <div>
+                    <h2 className='text-sm font-semibold'>LeanBoard</h2>
+                    <p className='text-xs'>Lean Board is a collaborative whiteboard with
                          sticky notes which seamlessly connects with your
                          GitHub issue.</p>
+                    </div>
                 </div>
-                <div>
-                    <h2>ODIN</h2>
-                    <p>The top apps in the world relay on Instabug for bug
+
+                <div className='flex items-center lg:w-4/6 gap-4'>
+                    <Image src={gamma} alt='gamma' className='w-10 h-10 rounded-full'/>
+                    <div>
+                    <h2 className='text-sm font-semibold'>Gammma Data Leak Prevention</h2>
+                    <p className='text-xs'>69% of data branches start with negligence. one
+                        negligent employee could cost your company a 
+                        decade worth of reputataion.
+                    </p>
+                    </div>
+                    
+                </div>
+               
+                
+                <div className='flex items-center lg:w-4/6 gap-4'>
+                    <Image src={ocd} alt='ocd' className='w-10 h-10 rounded-full'/>
+                    <div>
+                    <h2 className='text-sm font-semibold'>ODIN</h2>
+                    <p className='text-xs'>The top apps in the world relay on Instabug for bug
                         reporting, crash reporting, and in-app user 
                         feedback.
                     </p>
+                    </div>
                 </div>
 
             </div>
-            <div>
-                <button>View all</button>
+            <div className='text-center'>
+                <button className='px-6 py-2 bg-black text-white text-xs font-semibold'>View all</button>
             </div>
         </div>
     </div>
