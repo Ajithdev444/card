901aa146fcbb598e29d511f6e7dd9a9464ffe783
waywedesign <ajithdev.c@waywedesign.com>
2023-02-22 15:28:09 +0530

    width setting

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -10,19 +10,22 @@ import ocd from '../../public/ocd.jpg'
 const index = () => {
   return (
     <div className='bg-[#10182F] h-screen flex justify-center items-center'>
-        <div className='bg-white h-4/5 w-10/12 rounded-md'>
-            <div className='text-center py-6'>
-                <h1 className='text-base font-bold' >Recently added</h1>
-                <p className='text-[10px]'>The latest tools that help you and your team
+            <div className='bg-white rounded-md max-w-[768px] max-h-[1200px] p-24 2xl:max-w-7xl 2xl:p-40'>
+            <div className='text-center'>
+                <h1 className='text-lg font-bold'>Recently added</h1>
+                <p className='text-sm lg:mt-2'>The latest tools that help you and your team
                     building software better,together.
                 </p>
             </div>
-            <div className='grid grid-cols-2 py-10 px-48 gap-8'>
+            <div className='grid grid-cols-2 gap-8'>
                 <div className='flex items-center lg:w-5/6 gap-4'>
-                    <Image src={instabug} alt='instabug' className='w-12 h-12 rounded-full'/>
+                    <div className='bg-gray-200 rounded-full p-5 shrink-0'>
+                    <Image src={instabug} alt='instabug' className='w-14 h-14'/>
+                    </div>
+                   
                     <div>     
-                    <h2 className='text-sm font-semibold'>Instabug</h2>
-                    <p className='text-[10px]'>The top apps in the world rely on instabug for bug
+                    <h2 className='text-base font-semibold'>Instabug</h2>
+                    <p className='text-sm'>The top apps in the world rely on instabug for bug
                         reporting, crash reporting, and in-app user 
                         feedback.
                     </p>
@@ -30,10 +33,13 @@ const index = () => {
                 </div>
 
                 <div className='flex items-center lg:w-5/6 gap-4'>
-                    <Image src={deepscan} alt='deepscan' className='w-12 h-12 rounded-full'/>
+                    <div className='bg-blue-800 rounded-full p-5 shrink-0'>
+                      <Image src={deepscan} alt='deepscan' className='w-12 h-12 rounded-full'/>
+                    </div>
+                   
                     <div>
-                    <h2 className='text-sm font-semibold'>DeepScan</h2>
-                    <p className='text-[10px]'>DeepScan is an advanced static analysis tools
+                    <h2 className='text-base font-semibold'>DeepScan</h2>
+                    <p className='text-sm'>DeepScan is an advanced static analysis tools
                         engineered to support JavaScript, TypeSCRIPT,
                         React, and Vue.js.
                     </p>
@@ -41,10 +47,13 @@ const index = () => {
                 </div>
 
                 <div className='flex items-center lg:w-5/6 gap-4'>
+                    <div className='bg-black rounded-full p-5 shrink-0'>
                     <Image src={octobox} alt='octobox' className='w-12 h-12 rounded-full'/>
+                    </div>
+                   
                     <div>
-                    <h2 className='text-sm font-semibold'>Octobox</h2>
-                    <p className='text-[10px]'>Octobox helps you manage your notifications in 
+                    <h2 className='text-base font-semibold'>Octobox</h2>
+                    <p className='text-sm'>Octobox helps you manage your notifications in 
                         the same way Gmail helps you manage your 
                         email.
                     </p>
@@ -53,20 +62,26 @@ const index = () => {
                 </div>
 
                 <div className='flex items-center lg:w-5/6 gap-4'>
-                    <Image src={leanboard} alt='leanboard' className='w-12 h-12 rounded-full'/>
+                    <div className='bg-gray-200 rounded-full p-5 shrink-0'>
+                       <Image src={leanboard} alt='leanboard' className='w-12 h-12 rounded-full'/>
+                    </div>
+                    
                     <div>
-                    <h2 className='text-sm font-semibold'>LeanBoard</h2>
-                    <p className='text-[10px]'>Lean Board is a collaborative whiteboard with
+                    <h2 className='text-base font-semibold'>LeanBoard</h2>
+                    <p className='text-sm'>Lean Board is a collaborative whiteboard with
                          sticky notes which seamlessly connects with your
                          GitHub issue.</p>
                     </div>
                 </div>
 
                 <div className='flex items-center lg:w-5/6 gap-4'>
-                    <Image src={gamma} alt='gamma' className='w-12 h-12 rounded-full'/>
+                    <div className='bg-gray-200 rounded-full p-5 shrink-0'>
+                       <Image src={gamma} alt='gamma' className='w-12 h-12 rounded-full'/>
+                    </div>
+                   
                     <div>
-                    <h2 className='text-sm font-semibold'>Gammma Data Leak Prevention</h2>
-                    <p className='text-[10px]'>69% of data branches start with negligence. one
+                    <h2 className='text-base font-semibold'>Gammma Data Leak Prevention</h2>
+                    <p className='text-sm'>69% of data branches start with negligence. one
                         negligent employee could cost your company a 
                         decade worth of reputataion.
                     </p>
@@ -76,10 +91,12 @@ const index = () => {
                
                 
                 <div className='flex items-center lg:w-4/6 gap-4'>
+                    <div className='bg-gray-200 rounded-full p-5 shrink-0'>
                     <Image src={ocd} alt='ocd' className='w-10 h-10 rounded-full'/>
+                    </div>
                     <div>
-                    <h2 className='text-sm font-semibold'>ODIN</h2>
-                    <p className='text-[10px]'>The top apps in the world relay on Instabug for bug
+                    <h2 className='text-base font-semibold'>ODIN</h2>
+                    <p className='text-sm'>The top apps in the world relay on Instabug for bug
                         reporting, crash reporting, and in-app user 
                         feedback.
                     </p>
@@ -88,7 +105,7 @@ const index = () => {
 
             </div>
             <div className='text-center'>
-                <button className='px-6 py-2 bg-black text-white text-xs font-semibold'>View all</button>
+                <button className='px-6 py-2 mb-4 bg-black text-white text-xs font-semibold'>View all</button>
             </div>
         </div>
     </div>
