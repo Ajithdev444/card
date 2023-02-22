460b9081ad4dff833944677734a31e9f1f5881d9
waywedesign <ajithdev.c@waywedesign.com>
2023-02-22 09:41:12 +0530

    background change

--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div className='bg-gray-300 grid h-screen'>
+    <div className='bg-[#F4F9FF] grid h-screen'>
         <div className='bg-white p-10 w-80 h-auto text-center m-auto'>
   
           <div>
             <h1 className='text-xl font-bold mb-3'>Join the<br/>community</h1>
             <p className='text-xs mb-3'>Take your art to the next level.Get it
               seen by millions of people.</p>
-              <button className='bg-blue-800 text-xs font-semibold w-full text-white px-8 py-2 rounded-md'>Join with Facebook</button>
+              <button className='bg-[#4A68AE] text-xs font-semibold w-full text-white px-8 py-2 rounded-md'>Join with Facebook</button>
               <p className='mb-3 mt-3 text-xs'>Or</p>
           </div>
           <div className='grid grid-cols-2 gap-2 mb-3'>
@@ -19,7 +19,7 @@ const Form = () => {
           <div className='grid grid-cols-1 gap-2 mb-3'>
             <input  className='px-2 py-2 text-xs font-semibold border'  type='email' placeholder='Email'/>
             <input  className='px-2 py-2 text-xs font-semibold border'  type='Password' placeholder='Password'/>
-            <button  className='bg-black text-xs font-semibold w-full text-white px-8 py-2 rounded-md'>Create New Account</button>
+            <button  className='bg-[#1D2C4B] text-xs font-semibold w-full text-white px-8 py-2 rounded-md'>Create New Account</button>
           </div>
           <div>
             <p className='text-[8px] p-3'>By joining, you agree to our Terms of<br/>
