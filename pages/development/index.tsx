88ca94989eaff5696e14e19aaf09f844899409b5
waywedesign <ajithdev.c@waywedesign.com>
2023-02-24 15:56:45 +0530

    div come out in heading

--- pages/development/index.tsx
+++ pages/development/index.tsx
@@ -5,12 +5,15 @@ import de from '../../public/de.jpeg'
 const Development = () => {
   return (
     <div className='relative bg-gray-300 min-h-screen flex  items-center justify-center'>
-        <div className='bg-[#FFF] rounded-2xl p-20 max-w-4xl flex'>
-            <div className='w-3/4'>
-            <div className='mb-4'>
+        
+        <div className='bg-[#FFF] rounded-2xl p-20 max-w-4xl flex flex-col '>
+        <div className='mb-4'>
             <h1 className='text-base font-semibold text-[#272A3D]'>Development</h1>
             <h2 className='text-xl font-bold text-[#323248] font-serif'>Compatible Inkjet Cartridge</h2>
             </div>
+            <div className='flex flex-row'>
+            <div className='w-3/4'>
+            
             <div>
                 <Image src={de} alt='development' className='w-full h-56 object-cover rounded-md'/>
             </div>
@@ -39,13 +42,14 @@ const Development = () => {
                     Read more</button>
             </div>
             </div>
-            <div className='bg-[#151D4B] rounded-md min-h-xl w-1/4 shadow-2xl p-8'>
+            <div className='bg-[#151D4B] rounded-md max-h-96 w-1/4 shadow-2xl p-8 mx-4'>
                 <h2 className='text-base font-bold text-white font-serif'>Party Jokes Starting But Unnecessary</h2>
                 <p className='text-sm text-white 2xl:mt-6'>Life is a journey of twists and turns,peaks and
                         valleys, mountains to climb and oceans to explore.</p>
                  <button className='px-4 py-2 border-[1px] border-[#000] rounded-md font-semibold text-sm bg-white mt-12'>
                     Read more</button>       
 
+            </div>
             </div>
          
  
