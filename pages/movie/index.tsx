24c29536404f484dea53614c80984bb8ca1d84ef
waywedesign <ajithdev.c@waywedesign.com>
2023-02-26 16:06:36 +0530

    css

--- pages/movie/index.tsx
+++ pages/movie/index.tsx
@@ -5,19 +5,19 @@ import movie from '../../public/movie.jpg'
 
 const index = () => {
   return (
-    <div className='min-h-screen bg-[#F5F6FA] flex justify-center items-center'>
-        <div className='max-w-7xl bg-[#FFFFF]'>
+    <div className='min-h-screen bg-gray-300 flex justify-center items-center p-10'>
+        <div className='max-w-5xl bg-[#FFF] p-20'>
             <div>
-                <h1>Star Wars:The Last Jedi</h1>
-                <Link href="/movies">Action & Adventure</Link>
+                <h1 className='text-2xl font-bold'>Star Wars:The Last Jedi</h1>
+                <Link href="/movies" className='text-[#3A6BA7] text-lg'>Action & Adventure</Link>
                 <div>7.4/10</div>
             </div>
 
             <div className='flex'>
-                <Image src={movie} alt='movie' className='w-36 h-60'/>
-                <div>
-                <h2>About the Movie</h2>
-                <p>In Lucasfilm's Star Wars:The Last jedi, the Skywalker saga
+                <Image src={movie} alt='movie' className='w-60 h-96'/>
+                <div className='ml-10'>
+                <h2 className='text-xl font-bold'>About the Movie</h2>
+                <p className='text-lg'>In Lucasfilm's Star Wars:The Last jedi, the Skywalker saga
                     continues as the heros of The Force Awakens join the galactic
                     legands in an epic adventure that unlocks new mysteries of the
                     Force and shocking revelations of the past.
@@ -25,31 +25,31 @@ const index = () => {
                 <div>
 
                 </div>
-                <div className='flex'>
+                <div className='flex mt-10 gap-10'>
                 <div className='flex flex-col'>
-                <h2>Actors</h2> 
-                <Link href='#'>Oscar Isaac</Link>
-                <Link href='#'>Mark Hamil</Link>
-                <Link href='#'>Daisy Ridley</Link>
-                <Link href='#'>John Boyega</Link>
-                <Link href='#'>Kelly Marie Tran</Link>
+                <h2 className='text-xl font-bold pb-2'>Actors</h2> 
+                <Link className='text-md text-[#3A6BA7]' href='#'>Oscar Isaac</Link>
+                <Link className='text-md text-[#3A6BA7]' href='#'>Mark Hamil</Link>
+                <Link className='text-md text-[#3A6BA7]' href='#'>Daisy Ridley</Link>
+                <Link className='text-md text-[#3A6BA7]' href='#'>John Boyega</Link>
+                <Link className='text-md text-[#3A6BA7]' href='#'>Kelly Marie Tran</Link>
                 </div>
                 <div className=''>
-                    <h2>Director</h2>
-                    <Link href='#'>Rian Johnson</Link>
-                    <div className=''>
-                    <h2>Screenwriter</h2>
-                    <Link href='#'>Rian Johnson</Link>
+                    <h2 className='text-xl font-bold pb-2'>Director</h2>
+                    <Link className='text-md text-[#3A6BA7]' href='#'>Rian Johnson</Link>
+                    <div className='mt-6'>
+                    <h2 className='text-xl font-bold pb-2'>Screenwriter</h2>
+                    <Link className='text-md text-[#3A6BA7]' href='#'>Rian Johnson</Link>
                 </div>
                 </div>
                 <div className='flex flex-col'>
-                    <h2>Producers</h2>
-                    <Link href='#'>Kathleen Kennedy</Link>
-                    <Link href='#'>Ram Bergman</Link>
+                    <h2 className='text-xl font-bold pb-2'>Producers</h2>
+                    <Link className='text-md text-[#3A6BA7]' href='#'>Kathleen Kennedy</Link>
+                    <Link className='text-md text-[#3A6BA7]' href='#'>Ram Bergman</Link>
                 </div> 
                 </div>
                 <div className=''>
-                <button>More</button>
+                <button className='text-lg text-[#3A6BA7] pb-4'>More</button>
                 </div>
                 </div>
                
@@ -61,9 +61,9 @@ const index = () => {
             </div>
 
             <div className='flex'>
-                <div>$19.00</div>
-                <button>Buy</button>
-                <button>WATCH TRAILER</button>
+                <div className='text-xl font-bold'>$19.00</div>
+                <button className='text-xl font-bold ml-28'>Buy</button>
+                <button className='text-xl font-bold place-items-end'>WATCH TRAILER</button>
                 
             </div>
 
