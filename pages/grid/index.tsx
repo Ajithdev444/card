c0d9bbef7bb8bf7d82ee836f3315935557d86105
waywedesign <ajithdev.c@waywedesign.com>
2023-03-01 14:41:15 +0530

    grid image alignment

--- pages/grid/index.tsx
+++ pages/grid/index.tsx
@@ -5,47 +5,40 @@ import gamma from '../../public/gamma.jpg'
 import google from '../../public/google.png'
 import img1 from '../../public/img1.jpg'
 import img2 from '../../public/img2.jpg'
+import movie from '../../public/movie.jpg'
+import uber from '../../public/uber.jpg'
 
 
 const index = () => {
   return (
     
         <div className='min-h-screen p-10 mx-auto justify-center flex items-center bg-[#C1C0BC]'>
-          <div className='gap-4 grid-cols-1 grid md:grid-cols-8 bg-white md:grid-rows-5 grid-flow-row max-w-6xl'>
-          <Image src={img1} alt='img1' className='row-span-2 w-full object-cover'/>
-          <Image src={google} alt='google' className='row-span-2 col-span-2 w-full h-32 object-cover'/>
-          <Image src={gamma} alt='gamma' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={deepscan} alt='deepscan' className='col-span-2 w-full h-32 object-cover'/>
-          <Image src={img1} alt='img1' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={img2} alt='img2' className='col-span-1 w-full h-32 object-cover '/>
+          <div className='gap-4 grid-cols-1 grid md:grid-cols-8 bg-white md:grid-rows-5 grid-flow-row-dense p-4 max-h-screen max-w-6xl'>
+          <Image src={img1} alt='img1' className='col-span-1 row-span-1 h-full w-full object-cover'/>
+          <Image src={google} alt='google' className='col-span-1 h-full object-cover'/>
+          <Image src={gamma} alt='gamma' className='col-span-1 h-full object-cover'/>
+          <Image src={deepscan} alt='deepscan' className='col-span-1 row-span-2 h-full object-cover'/>
+          <Image src={img1} alt='img1' className='col-span-1 h-full object-cover'/>
+          <Image src={img2} alt='img2' className='col-span-2 h-full object-cover '/>
+          <Image src={movie} alt='movie' className='col-span-1 row-span-2 h-full object-cover'/>
 
-          <Image src={img1} alt='img1' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={google} alt='google' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={gamma} alt='gamma' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={deepscan} alt='deepscan' className='col-span-2 w-full h-32 object-cover'/>
-          <Image src={img1} alt='img1' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={img2} alt='img2' className='col-span-2 w-full h-32 object-cover '/>
+          <Image src={img1} alt='img1' className='col-span-2 h-full object-cover'/>
+          <Image src={img2} alt='img2' className='col-span-1 row-span-2 h-full object-cover'/>
+          <Image src={movie} alt='movie' className='col-span-2 row-span-2 h-full object-cover'/>
+          <Image src={deepscan} alt='deepscan' className='col-span-1 h-full object-cover'/>
+          <Image src={img1} alt='img1' className='col-span-2 row-span-2 h-full object-cover'/>
+          <Image src={img2} alt='img2' className='col-span-2 row-span-4 h-full object-cover '/>
 
-          <Image src={img1} alt='img1' className='col-span-2 w-full h-32 object-cover'/>
-          <Image src={google} alt='google' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={gamma} alt='gamma' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={deepscan} alt='deepscan' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={img1} alt='img1' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={img2} alt='img2' className='col-span-2 w-full h-32 object-cover '/>
+          <Image src={img1} alt='img1' className='col-span-1 h-full object-cover'/>
+          <Image src={img2} alt='img2' className='col-span-2 row-span-2 h-full object-cover'/>
+          <Image src={uber} alt='uber' className='col-span-1 row-span-2 h-full object-cover'/>
+          <Image src={movie} alt='movie' className='col-span-1 h-full object-cover'/>
+          <Image src={img2} alt='img2' className='col-span-1 h-full object-cover'/>
+          <Image src={img1} alt='img1' className='col-span-1 h-full object-cover'/>
+          <Image src={deepscan} alt='deepscan' className='col-span-1 h-full object-cover'/>
 
-          <Image src={img1} alt='img1' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={google} alt='google' className='col-span-2 w-full h-32 object-cover'/>
-          <Image src={gamma} alt='gamma' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={deepscan} alt='deepscan' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={img1} alt='img1' className='col-span-2 w-full h-32 object-cover'/>
-          <Image src={img2} alt='img2' className='col-span-1 w-full h-32 object-cover '/>
-
-          <Image src={img1} alt='img1' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={google} alt='google' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={gamma} alt='gamma' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={deepscan} alt='deepscan' className='col-span-1 w-full h-32 object-cover'/>
-          <Image src={img1} alt='img1' className='col-span-2 w-full h-32 object-cover'/>
-          <Image src={img2} alt='img2' className='col-span-2 w-full h-32 object-cover '/>  
+        
+ 
         </div>
         </div>
     
