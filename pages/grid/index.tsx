19a1883d6108dd1bc8c7f3e5c17c25db48ff119f
waywedesign <ajithdev.c@waywedesign.com>
2023-03-01 18:04:32 +0530

    image change

--- pages/grid/index.tsx
+++ pages/grid/index.tsx
@@ -15,8 +15,8 @@ const index = () => {
         <div className='min-h-screen p-10 mx-auto justify-center flex items-center bg-[#C1C0BC]'>
           <div className='gap-4 grid-cols-1 grid md:grid-cols-8 bg-white md:grid-rows-5 grid-flow-row-dense p-4 max-h-screen max-w-6xl'>
           <Image src={img1} alt='img1' className='col-span-1 row-span-1 h-full w-full object-cover'/>
-          <Image src={google} alt='google' className='col-span-1 h-full object-cover'/>
-          <Image src={gamma} alt='gamma' className='col-span-1 h-full object-cover'/>
+          <Image src={movie} alt='movie' className='col-span-1 h-full object-cover'/>
+          <Image src={img2} alt='img2' className='col-span-1 h-full object-cover'/>
           <Image src={deepscan} alt='deepscan' className='col-span-1 row-span-2 h-full object-cover'/>
           <Image src={img1} alt='img1' className='col-span-1 h-full object-cover'/>
           <Image src={img2} alt='img2' className='col-span-2 h-full object-cover '/>
