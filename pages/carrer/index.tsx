2f86dc9bbcf35ec228a2135ed161a2d048af36f2
waywedesign <ajithdev.c@waywedesign.com>
2023-03-03 18:02:58 +0530

    commit

---
+++ pages/carrer/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h2>Welcome to our Digital Marketing Company!&lt</h2>
+        <p>We offer a wide range of digital marketing services to help grow your business</p>
+        <Link href='/photo'>Photo</Link>
+    </div>
+  )
+}
+
+export default index
+
