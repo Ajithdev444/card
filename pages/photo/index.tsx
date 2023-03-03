e56bcfcf3033ab2efa38ccc343824d6f3304e084
waywedesign <ajithdev.c@waywedesign.com>
2023-03-03 18:00:55 +0530

    layout

--- pages/photo/index.tsx
+++ pages/photo/index.tsx
@@ -23,49 +23,49 @@ const index = () => {
   return (
     <div className='min-h-screen bg-[#1C2333] flex justify-center p-20'>
 
-       <div className='bg-white max-w-5xl'>
-        <div className='relative'>
-                    <div className='w-16 h-80 bg-gray-400 absolute left-full top-0 right-0'>
-                        <Image src={dots} alt='dots' className='w-10 h-10 mb-10 mt-10 ml-2'/>
+       <div className='bg-white max-w-5xl relative'>
+       
+                    <div className='w-16 h-72 bg-gray-400 absolute left-full top-0 right-0'>
+                        <Image src={dots} alt='dots' className='w-10 h-10 mb-10 mt-4 ml-2'/>
                         <Image src={alarm} alt='alarm' className='w-6 h-6 mb-10 ml-4'/>
                         <Image src={pencil} alt='pencil' className='w-6 h-6 mb-10 ml-4'/>
                         <Image src={share} alt='share' className='w-6 h-6 mb-10 ml-4'/>
-                    </div>
+                    
         </div> 
-            <div className='flex p-12'>
+            <div className='flex px-12'>
             <div className='w-1/4 relative'>
                 <Image src={photo} alt='photo' className='h-full object-cover absolute'/>
             </div>
-            <div className='flex flex-col w-3/4 p-10'>
-                <div className='flex'>
-                <div className='pr-32'>
+            <div className='flex flex-col w-3/4 px-10'>
+                <div className='flex border-b-2 p-4'>
+                <div className='w-full border-r-2'>
                     <h1 className='text-xl font-bold'>Joan Doe</h1>
                     
                     <h3 className='text-gray-400 text-xs'>Photography</h3>
                     <Link href='#' className='text-gray-400 text-xs'>Timisora, RO</Link>
                 </div>
-                <div className='w-0.5 h-full bg-gray-200'></div>
-                <div className='flex gap-x-10 pr-16'>
+                
+                <div className='flex gap-x-10 justify-end items-end w-full border-r-2 px-4'>
                     <div>
-                        <Image src={gallery} alt='gallery' className='w-10 h-10'/>
-                        <div className='text-lg font-bold'>127</div>
+                        <Image src={gallery} alt='gallery' className='w-6 h-6 ml-2'/>
+                        <div className='text-2xl font-bold'>127</div>
                     </div>
                     <div>
-                        <Image src={heart} alt='png' className='w-10 h-10'/>
-                        <div  className='text-lg font-bold'>354</div>
+                        <Image src={heart} alt='png' className='w-6 h-6 ml-2'/>
+                        <div  className='text-2xl font-bold'>354</div>
                     </div>
                     <div>
-                        <Image src={user} alt='user' className='w-10 h-10'/>
-                        <div  className='text-lg font-bold'>470</div>
+                        <Image src={user} alt='user' className='w-6 h-6 ml-2'/>
+                        <div  className='text-2xl font-bold'>470</div>
                     </div>
                 </div>
-                <div className='w-0.5 h-full bg-gray-200'></div>  
-                <div className='justify-end'>
-                    <button className='px-8 py-2 bg-[#FBB800] text-white'>Follow</button>
+                  
+                <div className='flex justify-center w-full'>
+                    <button className='px-10 py-2 bg-[#FBB800] text-white'>Follow</button>
                 </div>
                 
                 </div>
-                <div className='h-0.5 bg-gray-200'></div>
+        
                 <div className='flex mt-4'>
                 <div className=' max-w-sm'>
                 <h2 className='mb-4 text-xl font-bold'>About me</h2>
@@ -80,102 +80,112 @@ const index = () => {
                 </div>
                 <div className='px-20'>
                     <h2 className='mb-4 text-xl font-bold'>Skills</h2>
-                    <div className='relative'>
+                    <div className='flex'>
                     <h3 className='text-xs text-gray-600 leading-6'>Equipment</h3>
-                    <div className='w-14 h-1 bg-black absolute bottom-0'></div>
-                    <div className='rounded-full bg-black absolute w-3 h-3 right-4 top-5'></div>
-                    <div className='w-20 h-1 bg-gray-300 '></div>
-
+                    <div className='relative p-3'>
+                    <div className='w-14 h-1 bg-black absolute'></div>
+                    <div className='rounded-full bg-black absolute w-3 h-3 right-8 bottom-2 '></div>
+                    <div className='w-20 h-1 bg-gray-300'></div>
+                    </div>
                     </div>
 
-                    <div className='relative'>
+                    <div className='flex'>
                     <h3 className='text-xs text-gray-600 leading-6'>Editing</h3>
-                    <div className='w-14 h-1 bg-black absolute bottom-0'></div>
-                    <div className='rounded-full bg-black absolute w-3 h-3 right-4 top-5'></div>
+                    <div className='relative p-3'>
+                    <div className='w-14 h-1 bg-black absolute'></div>
+                    <div className='rounded-full bg-black absolute w-3 h-3 right-8 bottom-2'></div>
                     <div className='w-20 h-1 bg-gray-300 '></div>
-
                     </div>
-                    <div className='relative'>
+                    </div>
+
+                    <div className='flex'>
                     <h3 className='text-xs text-gray-600 leading-6'>Marketing</h3>
-                    <div className='w-10 h-1 bg-black absolute bottom-0'></div>
-                    <div className='rounded-full bg-black absolute w-3 h-3 right-7 top-5'></div>
+                    <div className='relative p-3'>
+                    <div className='w-10 h-1 bg-black absolute'></div>
+                    <div className='rounded-full bg-black absolute w-3 h-3 right-10 bottom-2'></div>
+                    <div className='w-20 h-1 bg-gray-300 '></div>
+                    </div>
+                    
+
+                    </div>
+                    <div className='flex'>
+                    <h3 className='text-xs text-gray-600 leading-6'>Retouching</h3>
+                    <div className='relative p-3'>
+                    <div className='w-8 h-1 bg-black absolute'></div>
+                    <div className='rounded-full bg-black absolute w-3 h-3 right-12 bottom-2'></div>
                     <div className='w-20 h-1 bg-gray-300 '></div>
 
                     </div>
-                    <div className='relative'>
-                    <h3 className='text-xs text-gray-600 leading-6'>Retouching</h3>
-                    <div className='w-8 h-1 bg-black absolute bottom-0'></div>
-                    <div className='rounded-full bg-black absolute w-3 h-3 right-9 top-5'></div>
-                    <div className='w-20 h-1 bg-gray-300 '></div>
+                    
                     </div>  
                 </div> 
                 </div>
            </div>
             </div>            
             
-            <div className='ml-12'>
+            <div className='ml-12 mt-4'>
                 <h2 className='text-xl font-bold'>Pictures</h2>
             </div>
-            <div className='gap-4 grid-cols-1 grid md:grid-cols-4 bg-white md:grid-rows-2 grid-flow-row-dense p-12 '>
-                <div className='relative'>
-                <Image src={bike} alt='bike' className='col-span-1 row-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+            <div className='gap-4 grid-cols-1 grid md:grid-cols-7 bg-white md:grid-rows-4 grid-flow-row-dense px-12 py-2 '>
+                <div className='relative col-span-2 row-span-2 '>
+                <Image src={bike} fill alt='bike' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️12 Likes</div>
                 </div>
                 </div>
 
-                <div className='relative'>
-                <Image src={langosta} alt='langosta' className='col-span-1 row-span-1 w-full object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-3 row-span-1'>
+                <Image src={langosta} fill alt='langosta' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️9 Likes</div>
                 </div>
                 </div>
 
-                <div className='relative'>
-                <Image src={female} alt='female' className='col-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-2 row-span-2'>
+                <Image src={female} fill alt='female' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️43 Likes</div>
                 </div>
                 </div>
 
-                <div className='relative'>
-                <Image src={flower} alt='flower' className='col-span-1 row-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-2 row-span-2'>
+                <Image src={flower} fill alt='flower' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️24 Likes</div>
                 </div>
                 </div>
 
-                <div className='relative'>
-                <Image src={girl} alt='girl' className='col-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-1 row-span-2'>
+                <Image src={girl} fill alt='girl' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️62 Likes</div>
                 </div>
                 </div>
 
-                <div className='relative'>
-                <Image src={kid} alt='kid' className='col-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-2 row-span-2'>
+                <Image src={kid} fill alt='kid' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️78 Likes</div>
                 </div>
                 </div>
                
-                <div className='relative'>
-                <Image src={coffe} alt='coffe' className='col-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-2 row-span-2'>
+                <Image src={coffe} fill  alt='coffe' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️36 Likes</div>
                 </div>
                 </div>
 
-                <div className='relative'>
-                <Image src={man} alt='man' className='col-span-1 object-cover h-full'/>
-                <div className='relative bg-black'>
+                <div className='relative col-span-3 row-span-1'>
+                <Image src={man} alt='man' className='object-cover'/>
+                <div className='bg-black'>
                 <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️98 Likes</div>
                 </div>
                 </div>
                 
                 
             </div>
-            <div className='flex justify-end mr-4'>
+            <div className='flex justify-end px-12 pb-4'>
                 <button className='px-4 py-2 bg-gray-200 text-sm'>Load more</button>
             </div> 
      
