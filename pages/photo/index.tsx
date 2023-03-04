4af76b719b7bbcfdc1060996571c5cfaa85b6f9c
waywedesign <ajithdev.c@waywedesign.com>
2023-03-04 11:41:38 +0530

    images

--- pages/photo/index.tsx
+++ pages/photo/index.tsx
@@ -5,18 +5,12 @@ import Link from 'next/link'
 import gallery from '../../public/gallery.png'
 import heart from '../../public/heart.png'
 import user from '../../public/user.png'
-import bike from '../../public/bike.jpg'
-import flower from '../../public/flower.jpg'
-import langosta from '../../public/langosta.jpg'
-import girl from '../../public/girl.jpeg'
-import kid from '../../public/kid.png'
-import coffe from '../../public/coffe.png'
-import female from '../../public/female.jpg'
 import man from '../../public/man1.jpg'
 import share from '../../public/share.png'
 import pencil from '../../public/pencil.png'
 import alarm from '../../public/alarm.png'
 import dots from '../../public/dots.png'
+import love from '../../public/love.svg'
 
 
 const index = () => {
@@ -37,7 +31,7 @@ const index = () => {
                 <Image src={photo} alt='photo' className='h-full object-cover absolute'/>
             </div>
             <div className='flex flex-col w-3/4 px-10'>
-                <div className='flex border-b-2 p-4'>
+                <div className='flex border-b-2 py-4'>
                 <div className='w-full border-r-2'>
                     <h1 className='text-xl font-bold'>Joan Doe</h1>
                     
@@ -67,7 +61,7 @@ const index = () => {
                 </div>
         
                 <div className='flex mt-4'>
-                <div className=' max-w-sm'>
+                <div className='max-w-sm'>
                 <h2 className='mb-4 text-xl font-bold'>About me</h2>
                 <p className='text-gray-400 text-xs'>Dolor sit amet,utamur efficiantur ius cu, ludus eligendi adipisci
                     ex usu, per ne putent regione philosophia, Saperet civibus
@@ -78,7 +72,7 @@ const index = () => {
                 </p>
                 
                 </div>
-                <div className='px-20'>
+                <div className='px-10'>
                     <h2 className='mb-4 text-xl font-bold'>Skills</h2>
                     <div className='flex'>
                     <h3 className='text-xs text-gray-600 leading-6'>Equipment</h3>
@@ -128,58 +122,74 @@ const index = () => {
             </div>
             <div className='gap-4 grid-cols-1 grid md:grid-cols-7 bg-white md:grid-rows-4 grid-flow-row-dense px-12 py-2 '>
                 <div className='relative col-span-2 row-span-2 '>
-                <Image src={bike} fill alt='bike' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1657694421550-4884e32662a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDEwNHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60'
+                 fill alt='bike' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️12 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4 '/>
+                <span className='absolute left-6 bottom-0 text-white'>12 Likes</span>
                 </div>
                 </div>
 
                 <div className='relative col-span-3 row-span-1'>
-                <Image src={langosta} fill alt='langosta' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1581229694739-6ab0d4608254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'
+                fill alt='langosta' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️9 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-3 object-cover w-4 h-4 '/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>9 Likes</div>
                 </div>
                 </div>
 
                 <div className='relative col-span-2 row-span-2'>
-                <Image src={female} fill alt='female' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1621525278689-4779d647a9f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDY4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60'
+        
+                fill alt='female' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️43 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4 '/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>43 Likes</div>
                 </div>
                 </div>
 
                 <div className='relative col-span-2 row-span-2'>
-                <Image src={flower} fill alt='flower' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1649188864561-97bae45969da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDIzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60'
+                fill alt='flower' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️24 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4 '/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>24 Likes</div>
                 </div>
                 </div>
 
                 <div className='relative col-span-1 row-span-2'>
-                <Image src={girl} fill alt='girl' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1627894566221-92392971a334?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+                 fill alt='girl' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️62 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4'/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>62 Likes</div>
                 </div>
                 </div>
 
                 <div className='relative col-span-2 row-span-2'>
-                <Image src={kid} fill alt='kid' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1599862256852-8e4b9541ed7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60' 
+                fill alt='kid' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️78 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4'/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>78 Likes</div>
                 </div>
                 </div>
                
                 <div className='relative col-span-2 row-span-2'>
-                <Image src={coffe} fill  alt='coffe' className='object-cover'/>
+                <Image src='https://images.unsplash.com/photo-1630040995437-80b01c5dd52d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZmZlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+                 fill  alt='coffe' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️36 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4'/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>36 Likes</div>
                 </div>
                 </div>
 
                 <div className='relative col-span-3 row-span-1'>
                 <Image src={man} alt='man' className='object-cover'/>
                 <div className='bg-black'>
-                <div className='absolute left-0 bg-black bottom-0 text-white p-2 '>❤️98 Likes</div>
+                <Image src={love} alt='love' className='absolute left-2 bottom-2 object-cover w-4 h-4'/>
+                <div className='absolute left-6 bottom-0 text-white p-2 '>98 Likes</div>
                 </div>
                 </div>
                 
