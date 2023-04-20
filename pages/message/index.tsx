a437cd7c269149296135d897126eab00d6af4a5a
waywedesign <ajithdev.c@waywedesign.com>
2023-04-20 12:11:11 +0530

    message

--- pages/message/index.tsx
+++ pages/message/index.tsx
@@ -8,29 +8,42 @@ import close from "../../public/icons8-close.svg";
 const index = () => {
   return (
     <div className="bg-[#302D40] min-h-screen flex justify-center items-center">
-      <div className="flex flex-col max-w-screen-xl mx-auto">
-        <div className="flex bg-[#FF6A6B] p-4 rounded-tl-md rounded-tr-md text-white">
-          <div className="rounded-full p-1 bg-gray-200 shrink-0 z-50">
-            <Image src={girl1} alt="girl1" className="w-10 h-10 rounded-full" />
-          </div>
-          <div className="rounded-full p-1 bg-gray-200 shrink-0 z-40">
-            <Image src={girl2} alt="girl1" className="w-10 h-10 rounded-full" />
-          </div>
-          <div className="rounded-full p-1 bg-gray-200 shrink-0 z-50">
-            <Image src={girl3} alt="girl1" className="w-10 h-10 rounded-full" />
+      <div className="flex flex-col max-w-screen-xl mx-auto w-96 h-auto">
+        <div className="flex bg-[#FF7271] p-6 rounded-tl-md rounded-tr-md text-white relative ">
+          <div className="">
+            <div className="rounded-full p-0.5 bg-gray-200 absolute z-10 left-4">
+              <Image
+                src={girl1}
+                alt="girl1"
+                className="w-8 h-8 rounded-full "
+              />
+            </div>
+            <div className="rounded-full p-0.5 bg-gray-200 absolute z-0 left-12">
+              <Image src={girl2} alt="girl1" className="w-8 h-8 rounded-full" />
+            </div>
+            <div className="rounded-full p-0.5 bg-gray-200 absolute left-20">
+              <Image src={girl3} alt="girl1" className="w-8 h-8 rounded-full" />
+            </div>
           </div>
 
-          <div className="ml-2">
-            <h1 className="text-sm font-semibold">Elnora, Leila, Lucinda</h1>
-            <h2 className="text-xs">We typically reply in a few minutes </h2>
+          <div className="ml-28 flex flex-col">
+            <h1 className="text-sm font-semibold text-gray-200">
+              Elnora, Leila, Lucinda
+            </h1>
+            <h2 className="text-[10px] text-gray-200">
+              We typically reply in a few minutes{" "}
+            </h2>
           </div>
         </div>
-        <div className="flex bg-[#282739] p-10 gap-4 h-96">
-          <div className="rounded-full p-1 bg-gray-200 shrink-0 h-fit">
-            <Image src={girl2} alt="girl2" className="rounded-full w-10 h-10" />
+        <div className="flex bg-[#282739] p-10 gap-4 h-80">
+          <div className="rounded-full p-0.5 bg-gray-200 shrink-0 h-fit">
+            <Image src={girl2} alt="girl2" className="rounded-full w-8 h-8" />
           </div>
 
-          <div className="text-white text-[10px] leading-4 p-4 bg-[#3A384D] rounded-md h-fit">
+          <div className="text-gray-400 text-xs leading-5 p-4 bg-[#3A384D] rounded-md h-fit w-full relative">
+            <div className="inline-block overflow-hidden absolute -ml-2 left-0 top-4">
+              <div className="h-3 w-3 origin-bottom-right rotate-45 transform  bg-[#3A384D]"></div>
+            </div>
             Hi there!
             <br />
             Looking to get started? i can help
@@ -38,21 +51,22 @@ const index = () => {
           </div>
         </div>
         <div className="p-8 bg-[#3A384D] rounded-bl-md rounded-br-md">
-          <p className="text-white">
+          <p className="text-gray-300 text-[10px] tracking-widest leading-5">
             By using the chat, I confirm that I have read the{" "}
             <span className="underline text-[#FF6A6B] cursor-pointer">
               privacy
-              <br /> policy
-            </span>
+              <br />
+              policy
+            </span>{" "}
             and do not object to the use of my personal data.
           </p>
-          <button className="text-[#FF6A6B] border border-[#FF6A6B] px-8 py-3 rounded-full mt-4">
+          <button className="text-[#FF6A6B] border border-[#FF6A6B] px-8 py-2 rounded-full mt-4">
             I agree
           </button>
         </div>
         <div className="flex justify-end">
           <div className="rounded-full bg-[#FF6A6B]  w-fit p-4 mt-6">
-            <Image src={close} alt="close" className="w-8 h-8" />
+            <Image src={close} alt="close" className="w-7 h-7" />
           </div>
         </div>
       </div>
