08c5916f9fcbb3be3286a3bbcd498b8b4c54d1ea
waywedesign <ajithdev.c@waywedesign.com>
2023-04-21 12:49:48 +0530

    review update

--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -4,35 +4,185 @@ const index = () => {
   return (
     <div className="bg-[#1D1C2C] flex justify-center items-center min-h-screen">
       <div className="max-w-screen-xl mx-auto bg-[#221F32]">
-        <div className="p-12 max-w-xl">
+        <div className="p-12 max-w-lg">
           <h1 className="text-xl font-bold text-[#E1E0E2] mb-14">Reviews</h1>
           <input
             type="text"
             placeholder="Search reviews"
-            className="bg-[#252637] border px-16 py-6 text-lg w-full"
+            className="bg-[#252637] text-[#E1E0E2] border px-16 py-4 text-sm w-full rounded-md"
           />
-          <div className="flex gap-4 mt-10 flex-wrap">
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+          <div className="flex gap-2 mt-10 flex-wrap">
+            <button className="text-[#6b696d] text-xs px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Experience
             </button>
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+            <button className="text-[#6b696d] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Quality
             </button>
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+            <button className="text-[#6b696d] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Design
             </button>
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+            <button className="text-[#6b696d] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Size
             </button>
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+            <button className="text-[#6b696d] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Features
             </button>
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+            <button className="text-[#6b696d] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Value
             </button>
-            <button className="text-[#E1E0E2] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
+            <button className="text-[#6b696d] text-sm px-6 py-3 rounded-md bg-[#1D1C2C] hover:bg-[#8783f4] hover:text-white">
               Replacement
             </button>
+          </div>
+          <div className="bg-[#2E2C41] rounded-md relative p-6 mt-28">
+            <div className="flex">
+              <div className="w-10 h-10 bg-[#D46D68] text-[#E1E0E2] flex justify-center items-center rounded-full">
+                J
+              </div>
+              <div className="text-[#E1E0E2] ml-4 flex justify-center items-center">
+                Jesse Hopkins
+              </div>
+
+              <div className="flex absolute top-8 right-4">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>First star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Second star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Third star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Fourth star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Fourth star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </div>
+            </div>
+            <p className="text-[#E1E0E2] text-xs pt-4 pb-4 tracking-wide">
+              Gorgeous design! Even more responsive than
+              <br />
+              the previous version. A pleasure to use!
+            </p>
+            <div className="flex relative">
+              <div className="text-[#6b696d] text-sm">Feb 13,2021</div>
+              <button className="text-[#6b696d] text-sm absolute right-0 border border-[#1D1C2C]">
+                Share
+              </button>
+            </div>
+          </div>
+          <div className="bg-[#2E2C41] rounded-md relative p-6 mt-6">
+            <div className="flex">
+              <div className="w-10 h-10 bg-yellow-500 text-[#E1E0E2] flex justify-center items-center rounded-full">
+                A
+              </div>
+              <div className="text-[#E1E0E2] ml-4 flex justify-center items-center">
+                Alice Banks
+              </div>
+
+              <div className="flex absolute top-8 right-4">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>First star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Second star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Third star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Fourth star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Fourth star</title>
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              </div>
+            </div>
+            <p className="text-[#E1E0E2] text-xs pt-4 pb-4 tracking-wide">
+              The device has a clean design, and the metal
+              <br />
+              housing feels study in my hands.Soft rounded
+              <br />
+              corners make it a pleasure yo look at.
+            </p>
+            <div className="flex relative">
+              <div className="text-[#6b696d] text-sm">Feb 13,2021</div>
+              <button className="text-[#6b696d] text-sm absolute right-0 border border-[#1D1C2C]">
+                Share
+              </button>
+            </div>
           </div>
         </div>
       </div>
