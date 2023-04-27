8215591c7ae12c1cabc60ea75f956758d81b44b7
waywedesign <ajithdev.c@waywedesign.com>
2023-04-27 17:46:47 +0530

    review

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "reactstrap";
 
 const PopUp = () => {
   const [close, setClose] = useState(true);
+  const [name, setName] = useState("");
+  const [review, setReview] = useState("");
+
+  const submitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ name, review }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center">
-      <div className="w-[600px] flex flex-col">
+      <Form className="w-[600px] flex flex-col">
         {close && (
           <div className="bg-white p-2 rounded-md">
             <div className="p-6 flex relative">
@@ -18,13 +34,32 @@ const PopUp = () => {
                 X
               </div>
             </div>
-            <input placeholder="Name" className="px-6 py-2" />
-            <input placeholder="Paragraph" className="px-6 py-2" />
-            <input placeholder="Date" className="px-6 py-2" />
-            <button className="px-6 py-2">Submit</button>
+            <label>Name</label>
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              className="px-6 py-2"
+            />
+            <label>Review</label>
+            <input
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+              placeholder="Review"
+              className="px-6 py-2"
+            />
+            {/* <input
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              placeholder="Date"
+              className="px-6 py-2"
+            /> */}
+            <button type="submit" onClick={submitComment} className="px-6 py-2">
+              Submit
+            </button>
           </div>
         )}
-      </div>
+      </Form>
     </div>
   );
 };
---
+++ pages/api/comments/index.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { comments } from "../../../data/comments.json";
+type Data = {
+  name: string;
+  review: string;
+  id: number;
+  message: string;
+};
+
+function handler(req: NextApiRequest, res: NextApiResponse<Data[]>) {
+  if (req.method === "GET") {
+    res.status(200).json({ message: "data" });
+  } else if (req.method === "POST") {
+    const name = req.body.name;
+    const review = req.body.review;
+    const userdata = {
+      id: new Date(),
+      name: name,
+      review: review,
+    };
+    comments.push(userdata);
+    res.status(201).json(userdata);
+    console.log(userdata);
+  }
+}
+export default handler;
